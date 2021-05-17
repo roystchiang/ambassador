77ee8bdcc39ab16c5e39ff112161656814da2f27
Alix Cook <alixcook@datawire.io>
2021-05-17 15:47:42 -0400

    oops need to also clean dirty manifests

---
+++ manifests/manifests.mk
@@ -0,0 +1,8 @@
+push-manifests:
+	$(OSS_HOME)/manifests/push_manifests.sh
+.PHONY: push-manifests
+
+# This should always be safe to run because the manifest yaml should all be generated
+clean-manifests:
+	@git restore $(OSS_HOME)/manifests/*/*.yaml
+.PHONY: clean-manifests
